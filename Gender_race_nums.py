8306409a30f02e2b75a4a5add5af5aeebe1b42d7
MichaelHyder <132848819+MichaelHyder@users.noreply.github.com>
2023-07-14 12:04:04 -0400

    Add files via upload

--- Gender_race_nums.py
+++ Gender_race_nums.py
@@ -14,24 +14,38 @@ import pandas as pd
 
 #Import Data file where filename is a string of the files name
 def read_file(filename):
-    """This functions reads in a file and turns it into a DataFrame
+    """Read file and make DataFrame
+    
+    This functions reads in a file and turns it into a DataFrame.
 
 """
     return pd.read_csv(filename)
 
 
 def percentage(data, total):
-    """This function takes in a subset of data points  and the total number of data points and 
+    """Make percentage of a subet.
     
+    This function takes in a subset of data points  and the total number of data points and 
     finds the percentage that the subset makes up of the total. 
 
 """
     return data/len(total) * 100
 
 def gen_gender_output(df):   
-    """This function takes a DataFrame and returns a new DataFrame with the count and percent of total
+    """Make final output table from DataFrame
     
-    for each subset of the Gender data. 
+    This function takes a DataFrame and returns a new DataFrame with the count and percent of total
+    for each subset of the Gender data. It finds the different names of the data points and groups 
+    them together to find the subset of the data set that each one makes up. Then it maps each group 
+    to its appropriate name for the output and creates the arrays that will label the data in the final
+    table. It then creates the final DataFrame that will be outputted and rouns the results to 2 
+    decimals.
+    
+    Args:
+        df: DataFrame of the original data that we are inputting
+        
+    Returns:
+        DataFrame with percentage and count of each data type.
 
 """
 
@@ -39,13 +53,14 @@ def gen_gender_output(df):
     genders = df_clean["Gender"].unique()
     counts = np.array([])
     grouped_gender = df_clean.groupby(df_clean.Gender)
+    
     for i in genders:
-        print(len(grouped_gender.get_group(i)))
         counts = np.append(counts, (len(grouped_gender.get_group(i))))
-    array = lambda:['MALE' if (x=='M') else 'FEMALE' if (x=='F') \
+        
+    labels = lambda:['MALE' if (x=='M') else 'FEMALE' if (x=='F') \
                 else "OTHER" if (x == "O") else "UNKNOWN" for x in genders]
     arrays = [np.array(["Gender" for x in counts]),
-              np.array(array())
+              np.array(labels())
               ]
     
     df_final = pd.DataFrame([[counts[0], percentage(counts[0], df_clean)], [counts[1], percentage(counts[1], df_clean)],
@@ -55,9 +70,8 @@ def gen_gender_output(df):
     
     
 df = read_file("gender_race.csv")
-print(df)
 print(gen_gender_output(df))
-#Group the data and find the number data points for each group
+
 
 
 
